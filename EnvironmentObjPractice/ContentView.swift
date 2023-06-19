9d98c146917bf141c49a05165f5074e00cd7e10e
torinunna <torinunna9588@gmail.com>
2023-06-19 18:14:56 +0900

    Initial Commit

---
+++ EnvironmentObjPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EnvironmentObjPractice
+//
+//  Created by YUJIN KWON on 2023/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EnvironmentObjPractice/EnvironmentObjPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjPracticeApp.swift
+//  EnvironmentObjPractice
+//
+//  Created by YUJIN KWON on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
