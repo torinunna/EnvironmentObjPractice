c699abe412a330f4b2a7feecf41176b5f0785f2d
torinunna <torinunna9588@gmail.com>
2023-06-21 00:38:19 +0900

    create Third View then presents from SecondView's navigationLink

--- EnvironmentObjPractice/SecondView.swift
+++ EnvironmentObjPractice/SecondView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct SecondView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+            NavigationLink {
+                ThirdView()
+            } label: {
+                Text("Third View")
+            }
+        .navigationTitle("Second View")
     }
 }
 
---
+++ EnvironmentObjPractice/ThirdView.swift
@@ -0,0 +1,20 @@
+//
+//  ThirdView.swift
+//  EnvironmentObjPractice
+//
+//  Created by YUJIN KWON on 2023/06/21.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThirdView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdView()
+    }
+}
